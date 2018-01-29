a00bf8d49f30cb2a2ed28838279366b46fa98f75
minamorl <minamorl@minamorl.com>
2018-01-30 02:13:00 +0900

    My first parser in C++

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
-int main() {
+#include <boost/spirit/include/qi.hpp>
+
+namespace xylitol
+{
+namespace core
+{
+void print(const char c)
+{
+  std::cout << c << std::endl;
+}
+}
+}
+
+int main()
+{
+  using namespace boost::spirit::qi;
+  std::string s = R"(<a>)";
+  auto fst = s.begin();
+  parse(fst, s.end(), '>' >> char_('a')[&xylitol::core::print]);
   return 0;
 }
